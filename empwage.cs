d807d49d7af7584af759c24dc369ee88f64062a2
N1vas <ggj.nivas.18@gmail.com>
2020-09-27 22:07:58 +0530

    [N1vas] Add: Added a check whether employee is present or not

---
+++ empwage.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace empwage
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to employee wage computation program on master branch"):
+
+            Random random = new Random();
+            int fullTime = 1;
+
+            int empStatus = new Random(0,2);
+            if (empStatus = fullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
