f27984253df64d5281c9206af49b52e620cc9f0b
N1vas <ggj.nivas.18@gmail.com>
2020-09-27 22:35:16 +0530

    [N1vas] Refactor : Daily Employee wage calculation is implemented

--- empwage.cs
+++ empwage.cs
@@ -10,16 +10,24 @@ namespace empwage
 
             Random random = new Random();
             int fullTime = 1;
+            int wagePerHour = 20;
+            int workHours = 0;
+            int totalEmpWage = 0;
 
             int empStatus = new Random(0,2);
             if (empStatus = fullTime)
             {
                 Console.WriteLine("Employee is present");
+                workHours = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                workHours = 0;
             }
+
+            totalEmpWage = workHours * wagePerHour;
+            Console.WriteLine("Total Employee Wage is :"+totalEmpWage);
         }
     }
 }
